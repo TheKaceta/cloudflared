ea3db1a7f4282339c11b22c632b3d2a00d1fadec
TheKaceta <95971342+TheKaceta@users.noreply.github.com>
2024-08-09 21:28:19 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Baixar o pacote cloudflared
+# Baixar o pacote .deb do Cloudflared
 curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
 
-# Instalar o pacote cloudflared
+# Instalar o pacote .deb
 sudo dpkg -i cloudflared.deb
 
-# Instalar o serviço cloudflared com o token especificado
+# Instalar o serviço do Cloudflared
 sudo cloudflared service install eyJhIjoiOWZmZmI2NTQ0NDIwZDYwZWQwYWE5MTRlYzQzZWExM2QiLCJ0IjoiZDg2MDQyMzQtOGNjNS00ZjFkLWI1M2ItZDVmN2QxMmQyMjFkIiwicyI6Ik1UQTNaamszTVdZdE56bGlNaTAwWWpjMExUaGlOamd0TWpObU9XSTFZVGN3TldaayJ9
-
-# Limpar o pacote baixado
-rm cloudflared.deb
-
-# Instalação concluída
-echo "Instalação do cloudflared concluída com sucesso!"
