5c856c9c2311894a6799e9b4a6029c48fca31f9d
TheKaceta <95971342+TheKaceta@users.noreply.github.com>
2024-08-09 21:46:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 # Baixar o pacote .deb do Cloudflared
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
-
-# Instalar o pacote .deb
-sudo dpkg -i cloudflared.deb
-
-# Instalar o serviço do Cloudflared
-sudo cloudflared service install eyJhIjoiOWZmZmI2NTQ0NDIwZDYwZWQwYWE5MTRlYzQzZWExM2QiLCJ0IjoiZDg2MDQyMzQtOGNjNS00ZjFkLWI1M2ItZDVmN2QxMmQyMjFkIiwicyI6Ik1UQTNaamszTVdZdE56bGlNaTAwWWpjMExUaGlOamd0TWpObU9XSTFZVGN3TldaayJ9
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+sudo apt update
